174116da605bff550a1b483d0c3d8bfd6eb57a65
exceptionnotfound <mpjones428@gmail.com>
2016-11-03 09:42:57 -0700

    Added first few objects

---
+++ BattleshipModellingPractice/Objects/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipModellingPractice.Objects
+{
+    public class Player
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ BattleshipModellingPractice/Objects/Ship.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipModellingPractice.Objects
+{
+    public class Ship
+    {
+        public ShipType ShipType { get; set; }
+    }
+}
